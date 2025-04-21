a8d1a626e0a77c14f5433a90c4dafe3e4c2be510
jingsk <jingsk129@gmail.com>
2025-04-21 16:45:43 -0700

    specified requirements and modularized package.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(name='monochalcogen_polarization',
+      version='1.1',
+      #package_dir={'becqsdr': 'utils'},
+      packages=['MXPol'],
+      install_requires = [
+        'ase>=3.23',
+        'matscipy>=1.1.0',
+        'matplotlib>=3.9.0',
+        'becqspr>=1.0',
+        'mace-torch>=0.3.12',     
+   ]
+)
