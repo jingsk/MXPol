8c284ebd7dec902e46135e21947c0bde8287cc04
jingsk <jingsk129@gmail.com>
2025-04-21 19:19:48 -0700

    comments.

--- MXPol/grid.py
+++ MXPol/grid.py
@@ -2,6 +2,10 @@ import numpy as np
 from io import config
 
 def wave_grid(n, A, waveform = 'smooth square'):
+    '''
+    create purturbation grid according to spec.
+    Currently supported lateral lattice strain in triangular, smooth square or uniform grid.
+    '''
     wl = n
     x = np.arange(n)
     if waveform =='triangular':    
@@ -11,6 +15,7 @@ def wave_grid(n, A, waveform = 'smooth square'):
             np.sin(10 * np.pi * x/wl) / 5**2
         )
     if waveform =='smooth square':
+        #rounding factor. This works well for 40x1 supercell lattice with 0.7 Ang amplitude
         d = wl /200
         grid = A * np.sin(2  * np.pi * x/wl) / (np.sqrt(d**2 + np.sin(2  * np.pi * x/wl)**2))
     #atoms_flexed.translate(dis_vec)
@@ -19,6 +24,7 @@ def wave_grid(n, A, waveform = 'smooth square'):
     return grid
 
 build_config = config['build']
+#a_grid here is defined in absolute
 a_grid = build_config['b'] + \
     np.abs(wave_grid(
         build_config['n_grid'], 
