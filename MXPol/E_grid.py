757cc56e35aeb2ce0b961b202968478123d12c28
jingsk <jingsk129@gmail.com>
2025-04-21 23:19:53 -0700

    E_grid is called from dynamics.py instead.

--- MXPol/E_grid.py
+++ MXPol/E_grid.py
@@ -5,17 +5,20 @@ from io import config
 dyn_config = config['dynamics']
 E_waveform = dyn_config['E_waveform']
 E_wavelength = dyn_config['E_wavelength_fs']
-E_wave_amp = dyn_config['E_wave_amp_mV_per_ang']
 tot_step = dyn_config['tot_step'] 
 
-if dyn_config['E_waveform'] == 'uniform':
-    E_grid = E_wave_amp * np.ones(tot_step+ 1)
+def E_grid(E_wave_amp):
+    if dyn_config['E_waveform'] == 'uniform':
+        grid = E_wave_amp * np.ones(tot_step+ 1)
 
-if dyn_config['E_waveform'] == 'square':
-    step_grid = np.arange(tot_step + 1)
-    E_grid = E_wave_amp * signal.square(2 * np.pi * step_grid / E_wavelength)
+    if dyn_config['E_waveform'] == 'square':
+        step_grid = np.arange(tot_step + 1)
+        grid = E_wave_amp * signal.square(2 * np.pi * step_grid / E_wavelength)
 
-if dyn_config['E_waveform'] == 'sine':
-    step_grid = np.arange(tot_step + 1)
-    E_grid = E_wave_amp * np.sin(2 * np.pi * step_grid / E_wavelength)
+    if dyn_config['E_waveform'] == 'sine':
+        step_grid = np.arange(tot_step + 1)
+        grid = E_wave_amp * np.sin(2 * np.pi * step_grid / E_wavelength)
+    else:
+        raise NotImplementedError('waveform not yet implemented')
+    return grid
 
